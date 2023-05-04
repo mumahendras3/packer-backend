466e6a71683b4614f2da2656830b7bd7908a22fa
Muhammad Mahendra Subrata <mumahendras3@gmail.com>
2023-05-04 21:32:33 +0700

    feat: exclude password field by default for User model

--- models/user.js
+++ models/user.js
@@ -11,7 +11,8 @@ const userSchema = new Schema({
   },
   password: {
     type: String,
-    required: [true, 'Password is required']
+    required: [true, 'Password is required'],
+    select: false
   },
   watchList: [{ type: Schema.Types.ObjectId, ref: 'Repo' }]
 });
