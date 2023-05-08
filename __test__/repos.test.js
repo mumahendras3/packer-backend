0ade495d1c3f20de015d4d1b6c5753e37995c31f
Muhammad Mahendra Subrata <mumahendras3@gmail.com>
2023-05-08 13:45:54 +0700

    test: add error tests to `POST /repos`

--- __test__/repos.test.js
+++ __test__/repos.test.js
@@ -26,17 +26,37 @@ let access_token;
 // Mock the axios module
 jest.mock('axios');
 
-// The mock response for when hitting GitHub APIs
-const mockResp = {
+// The mock responses for when hitting GitHub APIs
+// When getting the latest version
+const mockRespGetVersion = {
   data: {
     name: 'v1.0',
-    avatar_url: 'https://picsum.photos/200',
     assets: [{
       name: 'asset1',
       browser_download_url: 'asset1-download-url'
     }]
   }
 };
+// When getting the repo owner's avatar url
+const mockRespGetAvatarUrl = {
+  data: {
+    avatar_url: 'https://picsum.photos/200'
+  }
+};
+// When trying to add a repo with no releases
+const mockRespNotFound = {
+  response: {
+    status: 404,
+    data: {
+      message: 'Not Found'
+    }
+  }
+}
+// Register these mock responses (these order of invocation corresponds
+// with the order of axios request that will be executed at test time)
+axios.mockResolvedValueOnce(mockRespGetVersion);
+axios.mockResolvedValueOnce(mockRespGetAvatarUrl);
+axios.mockRejectedValueOnce(mockRespNotFound);
 
 beforeAll(async () => {
   // Insert the temporary user
@@ -56,9 +76,6 @@ afterAll(async () => {
 
 describe('POST /repos', () => {
   it(`should respond with the message "Repo successfully added" and should return the ObjectId of the newly added repo`, async () => {
-    // Mock the GitHub endpoint requests
-    axios.mockResolvedValue(mockResp);
-
     const res = await request(app)
       .post('/repos')
       .set('access_token', access_token)
@@ -68,6 +85,42 @@ describe('POST /repos', () => {
     // Compare the returned id with the actual one in database
     const repo = await Repo.findOne({ name: repo1.name, ownerName: repo1.ownerName });
     expect(res.body).toHaveProperty('id', repo._id.toString());
+  });
+
+  it(`should respond with the error message "Repo name is required"`, async () => {
+    const res = await request(app)
+      .post('/repos')
+      .set('access_token', access_token)
+      .send({ ownerName: repo1.ownerName });
+    expect(res.status).toBe(400);
+    expect(res.body).toHaveProperty('message', 'Repo name is required');
+  });
+
+  it(`should respond with the error message "Repo owner name is required"`, async () => {
+    const res = await request(app)
+      .post('/repos')
+      .set('access_token', access_token)
+      .send({ name: repo1.name });
+    expect(res.status).toBe(400);
+    expect(res.body).toHaveProperty('message', 'Repo owner name is required');
+  });
+
+  it(`should respond with the error message "Repo already exists"`, async () => {
+    const res = await request(app)
+      .post('/repos')
+      .set('access_token', access_token)
+      .send(repo1);
+    expect(res.status).toBe(400);
+    expect(res.body).toHaveProperty('message', 'Repo already exists');
+  });
+
+  it('should respond with the error message "No releases found for this repo"', async () => {
+    const res = await request(app)
+      .post('/repos')
+      .set('access_token', access_token)
+      .send({ name: 'a', ownerName: 'b' })
+    expect(res.status).toBe(400);
+    expect(res.body).toHaveProperty('message', 'No releases found for this repo');
   });
 });
 
@@ -80,11 +133,11 @@ describe('GET /repos', () => {
     expect(Array.isArray(res.body)).toBe(true);
     expect(res.body[0]).toHaveProperty('name', repo1.name);
     expect(res.body[0]).toHaveProperty('ownerName', repo1.ownerName);
-    expect(res.body[0]).toHaveProperty('currentVersion', mockResp.data.name);
-    expect(res.body[0]).toHaveProperty('latestVersion', mockResp.data.name);
+    expect(res.body[0]).toHaveProperty('currentVersion', mockRespGetVersion.data.name);
+    expect(res.body[0]).toHaveProperty('latestVersion', mockRespGetVersion.data.name);
     expect(res.body[0]).toHaveProperty('latestReleaseAssets');
     expect(Array.isArray(res.body[0].latestReleaseAssets)).toBe(true);
-    expect(res.body[0].latestReleaseAssets[0].name).toBe(mockResp.data.assets[0].name);
-    expect(res.body[0].latestReleaseAssets[0].url).toBe(mockResp.data.assets[0].browser_download_url);
+    expect(res.body[0].latestReleaseAssets[0].name).toBe(mockRespGetVersion.data.assets[0].name);
+    expect(res.body[0].latestReleaseAssets[0].url).toBe(mockRespGetVersion.data.assets[0].browser_download_url);
   });
 });
