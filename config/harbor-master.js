a034d21eaba9b782c5f9cc2e46e2073b6bbbaee5
Muhammad Mahendra Subrata <mumahendras3@gmail.com>
2023-05-08 10:33:29 +0700

    refactor: general cleanups to .env and harbor-master config

--- config/harbor-master.js
+++ config/harbor-master.js
@@ -2,14 +2,14 @@ const docker = require("harbor-master");
 
 let client;
 
-if (process.env.REMOTE_HOST && process.env.REMOTE_PORT) {
+if (process.env.DOCKER_REMOTE_HOST && process.env.DOCKER_REMOTE_PORT) {
   client = docker.Client({
-    host: process.env.REMOTE_HOST,
-    port: process.env.REMOTE_PORT,
+    host: process.env.DOCKER_REMOTE_HOST,
+    port: process.env.DOCKER_REMOTE_PORT,
   });
 } else {
   client = docker.Client({
-    socket: process.env.UNIX_SOCKET,
+    socket: process.env.UNIX_SOCKET || '/var/run/docker.sock',
   });
 }
 
